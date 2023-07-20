4f4fb19b8de040f014ed90e12c657b17aa021eb7
IkaRio198 <ikario198@gmail.com>
2023-07-20 11:57:35 +0200

    cleanup functions

--- include/functions.php
+++ include/functions.php
@@ -1,9 +1,53 @@
 <?php
+/**
+ * La fonction getCurrentPuzzleID() récupère l'ID de l'énigme actuel à partir de l'URL.
+ * 
+ * @return int L'ID de l'énigme actuel.
+ */
 function getCurrentPuzzleID() {
     return filter_var(basename($_SERVER['PHP_SELF']), FILTER_SANITIZE_NUMBER_INT);
 }
 
-function puzzleIsResolved($puzzleID) {
+/**
+ * La fonction vérifie si une énigme est résolue en vérifiant si son ID se trouve dans le tableau des
+ * énigmes résolues stockées dans la session.
+ * 
+ * @param puzzleID Le paramètre puzzleID est un paramètre facultatif qui représente l'ID de l'énigme. Si
+ * aucun puzzleID n'est fourni, il prendra par défaut la valeur renvoyée par la fonction
+ * getCurrentPuzzleID().
+ * 
+ * @return bool VRAI ou FAUX.
+ */
+function puzzleIsResolved($puzzleID = NULL) {
+    if ($puzzleID == NULL) {
+        $puzzleID = getCurrentPuzzleID();
+    }
     return in_array($puzzleID, $_SESSION["resolvedPuzzles"]) ? TRUE : FALSE;
+}
+
+/**
+ * La fonction renvoie une chaîne qui inclut l'ID de l'énigme actuel dans un titre de site Web.
+ * 
+ * @return string Une chaîne qui inclut l'ID de l'énigme actuel dans le titre du site Web.
+ */
+function setPageTitle() {
+    return "Énigme ".getCurrentPuzzleID()." • web.snt.nsi.xyz";
+}
+
+/**
+ * La fonction tickPuzzle vérifie si unz énigme est résolue et si ce n'est pas le cas, ajoute l'ID de
+ * l'énigme à la liste des énigmes résolues dans la session et actualise la page après 3 secondes.
+ * 
+ * @param puzzleID Le paramètre  est facultatif et représente l'ID de l'énigme qui doit être
+ * validée. Si aucun puzzleID n'est fourni, la fonction utilisera la fonction getCurrentPuzzleID() pour
+ * obtenir l'ID de l'énigme actuel.
+ */
+function tickPuzzle($puzzleID = NULL) {
+    if ($puzzleID == NULL) {
+        $puzzleID = getCurrentPuzzleID();
+    }
+    if (!puzzleIsResolved($puzzleID)) {
+        $_SESSION["resolvedPuzzles"][] = $puzzleID;
+    }
 }
 ?>
--- include/tickpuzzle.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!in_array(filter_var(basename($_SERVER['PHP_SELF']), FILTER_SANITIZE_NUMBER_INT), $_SESSION["resolvedPuzzles"])) {
-    $_SESSION["resolvedPuzzles"][] = filter_var(basename($_SERVER['PHP_SELF']), FILTER_SANITIZE_NUMBER_INT);
-}
-?>
