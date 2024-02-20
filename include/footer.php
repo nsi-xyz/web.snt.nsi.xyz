dcec6dc5efd69b44396a09b3dfde1b17b19ba7b3
ilyas-r <ikario198@gmail.com>
2024-02-20 19:40:51 +0100

    Added author in footer

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <?php
 echo '<div class="footer">
       <p class="footer-content">Une idée originale de Vincent ROBERT, enseignant de spécialité NSI au Lycée Louis Pasteur d\'Avignon.</p>
-      <p class="footer-content">Développé par Ilyas RAHMOUN en octobre 2023 dans le cadre d\'un projet de spécialité NSI.</p>
+      <p class="footer-content">Développé par Ilyas RAHMOUN et Thomas SANCHEZ dans l\'année 2023-2024 dans le cadre d\'un projet de spécialité NSI.</p>
       <div class="pure-menu-horizontal">
         <ul>
           <li class="pure-menu-item"><a href="https://nsi.xyz" class="pure-menu-link-hidden" target="_blank">nsi.xyz</a></li>
