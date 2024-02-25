4b9e42274f9b61dab122f53d95b0eedb4cf987b1
ilyas-r <ikario198@gmail.com>
2024-02-25 21:35:29 +0100

    cleanup nav and footer

--- include/footer.php
+++ include/footer.php
@@ -1,14 +1,11 @@
-<?php
-echo '<div class="footer">
-      <p class="footer-content">Une idée originale de Vincent ROBERT, enseignant de spécialité NSI au Lycée Louis Pasteur d\'Avignon.</p>
-      <p class="footer-content">Développé par Ilyas RAHMOUN et Thomas SANCHEZ dans l\'année 2023-2024 dans le cadre d\'un projet de spécialité NSI.</p>
-      <div class="pure-menu-horizontal">
-        <ul>
-          <li class="pure-menu-item"><a href="https://nsi.xyz" class="pure-menu-link-hidden" target="_blank">nsi.xyz</a></li>
-          <li class="pure-menu-item"> </li>
-          <li class="pure-menu-item"><a href="https://purecss.io" class="pure-menu-link-hidden" target="_blank">Pure CSS</a></li>
-        </ul>
-      </div>
-    </div>
-';
-?>
+<div class="footer">
+  <p class="footer-content">Une idée originale de Vincent ROBERT, enseignant de spécialité NSI au Lycée Louis Pasteur d'Avignon.</p>
+  <p class="footer-content">Développé par Ilyas RAHMOUN et Thomas SANCHEZ dans l'année 2023-2024 dans le cadre d'un projet de spécialité NSI.</p>
+  <div class="pure-menu-horizontal">
+    <ul>
+      <li class="pure-menu-item"><a href="https://nsi.xyz" class="pure-menu-link-footer" target="_blank">nsi.xyz</a></li>
+      <li class="pure-menu-item"> </li>
+      <li class="pure-menu-item"><a href="https://purecss.io" class="pure-menu-link-footer" target="_blank">Pure CSS</a></li>
+    </ul>
+  </div>
+</div>
--- panel/include/nav_panel.php
+++ panel/include/nav_panel.php
@@ -1,17 +1,18 @@
 <div id="menu">
   <div class="pure-menu">
-    <a class="pure-menu-heading" href="./index.php">Panneau d\'administration</a>
+    <a class="pure-menu-heading" href="./index.php">Panneau d'administration</a>
     <ul>
       <li>Utilisateur : <?php echo $_SESSION["user_logged_in"]["username"]; ?></li>
     </ul>
     <ul class="pure-menu-list">
       <li class="pure-menu-item"><a href="./session.php" class="pure-menu-link">&#x1F465; Ma session</a></li>
       <li class="pure-menu-item"><a href="./stats.php" class="pure-menu-link">&#x1F4CA; Statistiques de session</a></li>
-      <li class="pure-menu-item"><a href="./users.php" class="pure-menu-link">Gestion des comptes</a></li>
-      <li class="pure-menu-item"><a href="./trads.php" class="pure-menu-link">Gestion des traductions</a></li>
     </ul>
   </div>
   <div class="menu-bottom">
+    <li class="pure-menu-item"><a href="./sessions.php" class="pure-menu-link">&#x1F5C2;&#xFE0F; Parcourir les sessions</a></li>
+    <li class="pure-menu-item"><a href="./users.php" class="pure-menu-link-hidden">&#x2699;&#xFE0F; Gestion des comptes</a></li>
+    <li class="pure-menu-item"><a href="./trads.php" class="pure-menu-link-hidden">&#x1F5C3;&#xFE0F; Gestion des traductions</a></li>
     <li class="pure-menu-item-reset"><a class="pure-menu-link" id="logout">&#x1F6AA; Se déconnecter</a></li>
   </div>
 </div>
@@ -20,7 +21,6 @@ if (isset($_COOKIE["reset-js"])) {
   resetSession("../index.php");
 }
 ?>
-
 <script>
   document.getElementById("logout").addEventListener("click", function() {
     let date = new Date();
