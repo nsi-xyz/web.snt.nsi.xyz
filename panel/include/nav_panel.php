f1e38f30b709efed22a7e2bda6b7f5503af38404
ilyas-r <ikario198@gmail.com>
2024-03-01 20:54:13 +0100

    unhide panel users page

--- panel/include/nav_panel.php
+++ panel/include/nav_panel.php
@@ -11,7 +11,7 @@
   </div>
   <div class="menu-bottom">
     <li class="pure-menu-item"><a href="./sessions.php" class="pure-menu-link">&#x1F5C2;&#xFE0F; Parcourir les sessions</a></li>
-    <li class="pure-menu-item"><a href="./users.php" class="pure-menu-link-hidden">&#x2699;&#xFE0F; Gestion des comptes</a></li>
+    <li class="pure-menu-item"><a href="./users.php" class="pure-menu-link">&#x2699;&#xFE0F; Gestion des comptes</a></li>
     <li class="pure-menu-item"><a href="./trads.php" class="pure-menu-link-hidden">&#x1F5C3;&#xFE0F; Gestion des traductions</a></li>
     <li class="pure-menu-item-reset"><a class="pure-menu-link" id="logout">&#x1F6AA; Se déconnecter</a></li>
   </div>
