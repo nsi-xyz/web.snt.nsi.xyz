1d9e35c0f8c6f595aa68dfc9e31b2108d48fd8d6
ilyas-r <ikario198@gmail.com>
2024-08-21 12:19:51 +0200

    Using utf8mb4 in pdo

--- panel/include/db.php
+++ panel/include/db.php
@@ -3,7 +3,7 @@ $adresse = "localhost:3306";
 $user = "root";
 $password = "";
 $db_name = "xtjh1161_web_snt_nsi_xyz_alpha";
-$dsn = "mysql:host=$adresse;dbname=$db_name";
+$dsn = "mysql:host=$adresse;dbname=$db_name;charset=utf8mb4";
 try {
     $db = new PDO($dsn, $user, $password);
 } catch (PDOException $error) {
