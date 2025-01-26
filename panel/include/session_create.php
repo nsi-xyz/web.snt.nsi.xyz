454baa7b7a5751d58d8e9f5f8372f0f7fab55fe4
ilyas-r <ikario198@gmail.com>
2025-01-26 18:01:04 +0100

    A session lasting less than 5 minutes can no longer be created.

--- panel/include/session_create.php
+++ panel/include/session_create.php
@@ -2,6 +2,7 @@
 <p class="p-content">Vous n'avez actuellement aucune session en cours.</p>
 <h2 class="content-subhead">Créer une session</h2>
 <p class="p-content">Vous êtes sur le point de créer une session accessible pour vos élèves. Ci-dessous, vous pouvez configurer les paramètres liés à la session.</p>
+<error></error>
 <form method="POST" action="" class="pure-form">
     <h3 class="content-subhead">Durée de la session</h3>
     <input type="number" id="hours" name="session_duration_hours" value="1" min="0" max="2"/>
@@ -18,7 +19,11 @@ if (isset($_POST["session_duration_hours"], $_POST["session_duration_minutes"], 
     $m = $_POST["session_duration_minutes"];
     $s = $_POST["session_duration_seconds"];
     $session_duration = $h*3600 + $m*60 + $s;
+    if ($session_duration >= 500) {
     createSession($db, $id_user, $session_duration);
     echo '<script>window.location.replace(window.location.href);</script>';
+    } else {
+        throwError("Une session doit durer au moins 5 minutes.");
+    }
 }
 ?>
