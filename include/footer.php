8eece3f4ef13465c138247a81e92c0f1725b1237
ilyas-r <ikario198@gmail.com>
2025-01-27 21:49:05 +0100

    Bump to 2.5

--- include/footer.php
+++ include/footer.php
@@ -7,6 +7,8 @@
       <li class="pure-menu-item"><a href="https://nsi.xyz" class="pure-menu-link-footer" target="_blank"><?php echo traduction("external_nsixyz_name"); ?></a></li>
       <li class="pure-menu-item"> </li>
       <li class="pure-menu-item"><a href="https://purecss.io" class="pure-menu-link-footer" target="_blank"><?php echo traduction("external_purecss_name"); ?></a></li>
+      <li class="pure-menu-item"> </li>
+      <li class="pure-menu-item"><a href="https://nsi.xyz/nous-contacter/" class="pure-menu-link-footer" target="_blank"><?php echo "Contact"; ?></a></li>
     </ul>
   </div>
 </div>
