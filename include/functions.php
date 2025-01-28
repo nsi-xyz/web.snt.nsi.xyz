ab133f714e2ebcd39c6c624a02c290d3f26a39a0
ilyas-r <ikario198@gmail.com>
2025-01-28 11:59:15 +0100

    Nicknames must be at least 3 characters long

--- include/functions.php
+++ include/functions.php
@@ -335,6 +335,7 @@ function stopSession($database, $id_session) {
 function canJoinSession($pseudo, $id_session, $database) {
     $status = getRows($database, "sessions", "*", "id = $id_session")["status"];
     $sessionIsOpen = $status == 1 ? true : false;
+    $pseudoToShort = strlen($pseudo) < 3 ? true : false;
     $pseudoAlreadyUsed = rowsCount($database, "users_session", "id_session = $id_session AND pseudo = \"$pseudo\"") == 0 ? false : true;
     $pseudoIsVerifiedUser = false; // On ne peut pas rejoindre une session avec le pseudo d'un utilisateur ayant un compte
     foreach (getRows($database, "users", "username", "1") as $row) {
@@ -342,7 +343,7 @@ function canJoinSession($pseudo, $id_session, $database) {
             $pseudoIsVerifiedUser = true;
         }
     }
-    return $sessionIsOpen && !$pseudoAlreadyUsed && !$pseudoIsVerifiedUser;
+    return $sessionIsOpen && !$pseudoAlreadyUsed && !$pseudoIsVerifiedUser && !$pseudoToShort;
 }
 
 function joinSession($pseudo, $id_session, $database, $local_path) {
